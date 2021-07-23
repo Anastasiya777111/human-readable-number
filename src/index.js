6edd471ee7f4acb1cc1826082228f1d15df30f66
Anastasiya777111 <avojtesik1@gmail.com>
2021-07-23 12:48:16 +0300

    feat:
    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
-  
+    number = Number(number);
+    if (number == 0) {
+    return "zero";
+    }
+
+    let onenine = [0,"one", "two", "three", "four", "five", "six", "seven", "eight","nine",];
+    let tennineteen = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen","nineteen",];
+    let tenninety = [0, "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",];
+
+    let result = "";
+    if (number > 99) {
+    result = result + onenine[(number - (number % 100)) / 100] + " " + "hundred";
+    if (number % 100 == 0) {
+    return result;
+    }
+    }
+    if (number % 100 < 20 && number % 100 > 9) {
+        if (number > 100) {
+        result = result + " ";
+        }
+        result = result + tennineteen[number % 10];
+        return result;
+        }
+        if (number % 100 < 10 && number % 100 > 0) {
+        if (number > 100) {
+        result = result + " ";
+        }
+        result = result + onenine[number % 10];
+        return result;
+        }
+        if (number % 100 > 19) {
+        if (number > 100) {
+        result = result + " ";
+        }
+        result = result + tenninety[((number % 100) - (number % 10)) / 10];
+        if (number % 10 != 0) {
+        result = result + " " + onenine[number % 10];
+        }
+        return result;
+        }
+        };
 }
